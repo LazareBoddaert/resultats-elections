b92a7e13e1568a42144b024d087efde0761e1127
Lazare Boddaert <lazare.boddaert@gmail.com>
2022-11-21 15:44:49 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'resultats/index'
-      get 'resultats/show'
-      get 'votes/index'
-      get 'votes/show'
-      get 'scrutins/index'
-      get 'scrutins/show'
-      get 'candidats/index'
-      get 'candidats/show'
-      get 'formations_politiques/index'
-      get 'formations_politiques/show'
-      get 'communes/index'
-      get 'communes/show'
-      get 'departements/index'
-      get 'departements/show'
-      get 'regions/index'
-      get 'regions/show'
+      resources :resultats, only: %i[index show]
+      resources :votes, only: %i[index show]
+      resources :scrutins, only: %i[index show]
+      resources :candidats, only: %i[index show]
+      resources :formations_politiques, only: %i[index show]
+      resources :communes, only: %i[index show]
+      resources :departements, only: %i[index show]
+      resources :regions, only: %i[index show]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
