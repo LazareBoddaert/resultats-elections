907964ccfe7be14b7bd90e0fd955ebc6069b3f95
Lazare Boddaert <lazare.boddaert@gmail.com>
2022-12-02 13:49:05 +0100

    need to rework on the db schema

--- app/models/resultat_candidat.rb
+++ app/models/resultat_candidat.rb
@@ -1,4 +1,16 @@
 class ResultatCandidat < ApplicationRecord
   belongs_to :scrutin
   belongs_to :candidat
+
 end
+
+## Besoin de récupérée le nombre d'inscrit et de votant dans la commune
+# def score_pourcentage_inscrit(valeur_nb)
+#   @prc_ins = (valeur_nb * 100).fdiv(inscrit_nombre)
+#   @prc_ins.round(2)
+# end
+
+# def score_pourcentage_votant(valeur_nb)
+#   @prc_votant = (valeur_nb * 100).fdiv(votant_nombre)
+#   @prc_votant.round(2)
+# end
